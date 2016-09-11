7f062b1fe0d48d4facd7ab1ee7fb6be1f2eb50fa
klapatnyuk <klapatnyuk@yandex.ru>
2016-09-11 22:28:57 +0300

    Removed Stream API, small code optimization (continued)

--- src/main/java/ru/klapatniuk/grouper/lazy/Node.java
+++ src/main/java/ru/klapatniuk/grouper/lazy/Node.java
@@ -5,14 +5,14 @@ import java.util.*;
 /**
  * @author Viacheslav Klapatniuk
  */
-public class Node {
+class Node {
 
     private final Map<Integer, Node> children = new HashMap<>();
     private final Set<Word> group = new HashSet<>();
 
     private Word temp;
 
-    public void build(Word word) {
+    void build(Word word) {
         if (temp == null) {
             if (group.isEmpty() && children.isEmpty()) {
                 // node is empty
@@ -28,16 +28,16 @@ public class Node {
         complete(word);
     }
 
-    public List<Set<String>> getFlatGroups() {
+    List<Set<String>> getFlatGroups() {
         if (temp != null) {
-            return Collections.singletonList(Collections.singleton(temp.getOriginal()));
+            return Collections.singletonList(Collections.singleton(temp.toString()));
         }
 
         List<Set<String>> result = new ArrayList<>();
         if (!group.isEmpty()) {
             Set<String> strings = new HashSet<>();
             for (Word word : group) {
-                strings.add(word.getOriginal());
+                strings.add(word.toString());
             }
             result.add(strings);
         }
--- src/main/java/ru/klapatniuk/grouper/lazy/Word.java
+++ src/main/java/ru/klapatniuk/grouper/lazy/Word.java
@@ -5,15 +5,13 @@ import java.util.*;
 /**
  * @author Viacheslav Klapatniuk
  */
-public class Word {
+class Word {
 
-    private final String original;
     private final String lower;
     private final NavigableSet<Character> characters;
     private final Map<Character, Integer> hashes;
 
-    public Word(String original) {
-        this.original = original;
+    Word(String original) {
         this.lower = original.toLowerCase();
         this.characters = new TreeSet<>();
         this.hashes = new HashMap<>();
@@ -27,7 +25,7 @@ public class Word {
         }
     }
 
-    public Integer pollFirstCharacterHash() {
+    Integer pollFirstCharacterHash() {
         Character character = characters.pollFirst();
         if (character == null) {
             return null;
@@ -35,17 +33,14 @@ public class Word {
         return hashes.get(character);
     }
 
-    public String getOriginal() {
-        return original;
+    @Override
+    public String toString() {
+        return lower;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Word word = (Word) o;
-        return lower.equals(word.lower);
-
+        return lower.equals(((Word) o).lower);
     }
 
     @Override
